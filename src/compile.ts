d3f41dee4a862c31a1d0c388512c1deb6481303a
winpzs <winpzs@gmail.com>
2017-06-27 11:10:34 +0800

    CompileSubject add detach

--- src/compile.ts
+++ src/compile.ts
@@ -423,7 +423,8 @@ export interface ISubscribeParam {
     ready?: (p: ISubscribeEvent) => void;
     //节点或视图删除
     remove?: (p: ISubscribeEvent) => void;
-    isRemove?: boolean;
+    //分离节点
+    detach?: (p: ISubscribeEvent) => void;
 }
 
 export class CompileSubject {
@@ -435,6 +436,7 @@ export class CompileSubject {
                     init: (p: ISubscribeEvent) => (!exclude || !exclude.init) && this.init(p),
                     update: (p: ISubscribeEvent) => (!exclude || !exclude.update) && this.update(p),
                     ready: (p: ISubscribeEvent) => (!exclude || !exclude.ready) && this.ready(p),
+                    detach: (p: ISubscribeEvent) => (!exclude || !exclude.detach) && this.detach(p),
                     remove: (p: ISubscribeEvent) => (!exclude || !exclude.remove) && this.remove(p)
                 });
                 this.subject = subject;
@@ -450,6 +452,10 @@ export class CompileSubject {
         list.push(p[name]);
     }
 
+    /**
+     * 观察
+     * @param p 观察内容
+     */
     subscribe(p: ISubscribeParam): ISubscribeParam {
         if (!this.isRemove) {
             p.update && this.subscribeIn('update', p);
@@ -475,10 +481,15 @@ export class CompileSubject {
         }
     }
 
+    /**
+     * 解除观察
+     * @param p 观察内容
+     */
     unSubscribe(p: ISubscribeParam): void {
         if (!this.isRemove){
             p.update && this.unSubscribeIn('update', p);
             p.ready && this.unSubscribeIn('ready', p);
+            p.detach && this.unSubscribeIn('detach', p);
             p.remove && this.unSubscribeIn('remove', p);
             p.init && this.unSubscribeIn('init', p);
         }
@@ -486,13 +497,23 @@ export class CompileSubject {
 
     private linkParam: ISubscribeParam;
     private subject: CompileSubject;
+    /**
+     * 解除观察Subject
+     */
     unLinkSubject(): CompileSubject {
         this.subject && this.subject.unSubscribe(this.linkParam);
         return this;
     }
 
+    /**
+     * 是否已经初始化
+     */
     isInit: boolean = false;
     private initList:ISubscribeParam[];
+    /**
+     * 发送初始化通知
+     * @param p 发送事件参数
+     */
     init(p: ISubscribeEvent) {
         if (this.isRemove) return;
         this.isInit = true;
@@ -504,15 +525,42 @@ export class CompileSubject {
     }
 
     private updateList:ISubscribeParam[];
+    /**
+     * 发送更新通知
+     * @param p 发送事件参数
+     */
     update(p: ISubscribeEvent) {
-        if (this.isRemove) return;
+        if (this.isRemove || this.isDetach) return;
         CmpxLib.each(this.updateList, function (fn:any) {
             fn && fn(p);
         });
     }
 
+    /**
+     * 是否已分离
+     */
+    isDetach: boolean = false;
+    private detachList:ISubscribeParam[];
+    /**
+     * 发送分离通知，不删除
+     * @param p 发送事件参数
+     */
+    detach(p: ISubscribeEvent) {
+        if (this.isRemove) return;
+        CmpxLib.each(this.detachList, function (fn:any) {
+            fn && fn(p);
+        });
+    }
+
+    /**
+     * 是否已经准备
+     */
     isReady:boolean = false;
     private readyList:ISubscribeParam[];
+    /**
+     * 发送准备通知
+     * @param p 发送事件参数
+     */
     ready(p: ISubscribeEvent) {
         if (this.isRemove) return;
         var list = this.readyList;
@@ -522,8 +570,15 @@ export class CompileSubject {
         });
     }
 
+    /**
+     * 是否已经删除
+     */
     isRemove: boolean = false;
     private removeList:ISubscribeParam[];
+    /**
+     * 发送删除通知
+     * @param p 发送事件参数
+     */
     remove(p: ISubscribeEvent) {
         if (this.isRemove) return;
         this.isRemove = true;
